76a9b1b307046f41fbc15b303052d69a6075bc32
Ali <alipalvane@gmail.com>
2022-09-12 20:55:18 +0430

    add constants folder and dynamic data

--- src/components/exprience/Exprience.jsx
+++ src/components/exprience/Exprience.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./exprience.css";
-import { BsPatchCheckFill } from "react-icons/bs";
+import { frontExprience, backEndExprience } from "../../constants";
 
 const Exprience = () => {
   return (
@@ -11,97 +11,34 @@ const Exprience = () => {
         <div className="exprience__frontend">
           <h3>Front End Development</h3>
           <div className="exprience__content">
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>HTML</h4>
-                <small className="text-light">Exprienced</small>
-              </div>
-            </article>
-
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>CSS</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>Javascript</h4>
-                <small className="text-light">Exprienced</small>
-              </div>
-            </article>
-
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>Bootstarp</h4>
-                <small className="text-light">Exprienced</small>
-              </div>
-            </article>
-
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>Tailwind</h4>
-                <small className="text-light">Exprienced</small>
-              </div>
-            </article>
-
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>React</h4>
-                <small className="text-light">Exprienced</small>
-              </div>
-            </article>
+            {frontExprience.map(({ title, level, icon }, index) => {
+              return (
+                <article className="exprience__details" key={index}>
+                  {icon}
+                  <div>
+                    <h4>{title}</h4>
+                    <small className="text-light">{level}</small>
+                  </div>
+                </article>
+              );
+            })}
           </div>
         </div>
+
         <div className="exprience__backend">
           <h3>Back End Development</h3>
           <div className="exprience__content">
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>Node JS</h4>
-                <small className="text-light">Exprienced</small>
-              </div>
-            </article>
-
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>Mongo DB</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>Graqh QL</h4>
-                <small className="text-light">Exprienced</small>
-              </div>
-            </article>
-
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>Next JS</h4>
-                <small className="text-light">Exprienced</small>
-              </div>
-            </article>
-
-            <article className="exprience__details">
-              <BsPatchCheckFill className="exprience__details-icon"/>
-              <div>
-                <h4>Mongoose</h4>
-                <small className="text-light">Exprienced</small>
-              </div>
-            </article>
+            {backEndExprience.map(({ title, level, icon }, index) => {
+              return (
+                <article className="exprience__details" key={index}>
+                  {icon}
+                  <div>
+                    <h4>{title}</h4>
+                    <small className="text-light">{level}</small>
+                  </div>
+                </article>
+              );
+            })}
           </div>
         </div>
       </div>
---
+++ src/constants/index.js
@@ -0,0 +1,62 @@
+import { BsPatchCheckFill } from "react-icons/bs";
+export const frontExprience = [
+    {
+        title:'HTML',
+        level:'Exprienced',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+    {
+        title:'CSS',
+        level:'Intermediate',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+    {
+        title:'Javascript',
+        level:'Exprienced',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+    {
+        title:'Bootstarp',
+        level:'Exprienced',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+    {
+        title:'Tailwind',
+        level:'Exprienced',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+    {
+        title:'React',
+        level:'Exprienced',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    }
+]
+
+export const backEndExprience = [
+    {
+        title:'Node JS',
+        level:'Exprienced',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+    {
+        title:'Mongo DB',
+        level:'Intermediate',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+    {
+        title:'Graqh QL',
+        level:'Exprienced',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+    {
+        title:'Next JS',
+        level:'Exprienced',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+    {
+        title:'Mongoose',
+        level:'Exprienced',
+        icon: <BsPatchCheckFill className="exprience__details-icon"/>
+    },
+]
+
