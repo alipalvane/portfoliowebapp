370f5c65267aaeda10c447ef270ccbb8c79ef835
Ali <alipalvane@gmail.com>
2022-09-12 22:16:51 +0430

    dynamic data for about items

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import ME from '../../assets/me-about.jpg'
-import {TbAward, TbUsers, TbDeviceLaptop} from 'react-icons/tb'
-import './about.css';
+import React from "react";
+import ME from "../../assets/me-about.jpg";
+import "./about.css";
+import {aboutData} from "../../constants";
 
-
-
-const About = ()=> {
+const About = () => {
   return (
     <section id="about">
       <h5>Get To Know</h5>
@@ -13,38 +11,35 @@ const About = ()=> {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt="me about"/>
+            <img src={ME} alt="me about" />
           </div>
         </div>
         <div className="about__content">
           <div className="about__cards">
-            <article className="about__card">
-              <TbAward className='about__icon'/>
-              <h5>Exprience</h5>
-              <small>+3 Years Working</small>
-            </article>
-
-            <article className="about__card">
-              <TbUsers className='about__icon'/>
-              <h5>Clients</h5>
-              <small>200+ Worldwide</small>
-            </article>
-
-            <article className="about__card">
-              <TbDeviceLaptop className='about__icon'/>
-              <h5>Projects</h5>
-              <small>80+ Completed</small>
-            </article>
+            {aboutData.map(({icon, title, detail},index) => {
+              return (
+                <article key={index} className="about__card">
+                  {icon}
+                  <h5>{title}</h5>
+                  <small>{detail}</small>
+                </article>
+              );
+            })}
           </div>
 
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
 
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
-        
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,62 +1,80 @@
 import { BsPatchCheckFill } from "react-icons/bs";
+import { TbAward, TbUsers, TbDeviceLaptop } from "react-icons/tb";
 export const frontExprience = [
-    {
-        title:'HTML',
-        level:'Exprienced',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-    {
-        title:'CSS',
-        level:'Intermediate',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-    {
-        title:'Javascript',
-        level:'Exprienced',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-    {
-        title:'Bootstarp',
-        level:'Exprienced',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-    {
-        title:'Tailwind',
-        level:'Exprienced',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-    {
-        title:'React',
-        level:'Exprienced',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    }
-]
+  {
+    title: "HTML",
+    level: "Exprienced",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+  {
+    title: "CSS",
+    level: "Intermediate",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+  {
+    title: "Javascript",
+    level: "Exprienced",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+  {
+    title: "Bootstarp",
+    level: "Exprienced",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+  {
+    title: "Tailwind",
+    level: "Exprienced",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+  {
+    title: "React",
+    level: "Exprienced",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+];
 
 export const backEndExprience = [
-    {
-        title:'Node JS',
-        level:'Exprienced',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-    {
-        title:'Mongo DB',
-        level:'Intermediate',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-    {
-        title:'Graqh QL',
-        level:'Exprienced',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-    {
-        title:'Next JS',
-        level:'Exprienced',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-    {
-        title:'Mongoose',
-        level:'Exprienced',
-        icon: <BsPatchCheckFill className="exprience__details-icon"/>
-    },
-]
+  {
+    title: "Node JS",
+    level: "Exprienced",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+  {
+    title: "Mongo DB",
+    level: "Intermediate",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+  {
+    title: "Graqh QL",
+    level: "Exprienced",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+  {
+    title: "Next JS",
+    level: "Exprienced",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+  {
+    title: "Mongoose",
+    level: "Exprienced",
+    icon: <BsPatchCheckFill className="exprience__details-icon" />,
+  },
+];
 
+export const aboutData = [
+  {
+    icon: <TbAward className="about__icon" />,
+    title: "Exprience",
+    detail: "+3 Years Working",
+  },
+  {
+    icon: <TbUsers className="about__icon" />,
+    title: "Clients",
+    detail: "200+ Worldwide",
+  },
+  {
+    icon: <TbDeviceLaptop className="about__icon" />,
+    title: "Projects",
+    detail: "80+ Completed",
+  },
+];
